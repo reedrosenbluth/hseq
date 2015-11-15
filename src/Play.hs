9f68a60678e5fcc9f2d8d4f0229a97b2edf91d1a
Reed Rosenbluth <Reed.Rosenbluth@gmail.com>
2015-11-14 23:35:32 -0500

    can actually play sounds

--- src/Play.hs
+++ src/Play.hs
@@ -1,5 +1,12 @@
-module Play where
+module Play 
+  ( hitToMidiEvent
+  , play
+  )
+where
 
+import Control.Monad
+import Control.Monad.Trans
+import Control.Monad.Trans.State
 import Control.Lens
 import Control.Concurrent
 import System.MIDI
@@ -8,5 +15,44 @@ import System.Info
 import Types
 import Interpret
 
-play :: Connection -> Composition -> IO ()
-play = undefined
+hitToMidiEvent :: Hit -> MidiEvent
+hitToMidiEvent h = MidiEvent d (MidiMessage 10 (NoteOn t v))
+  where
+    t = 35 + fromEnum (h ^. tone)
+    d = fromIntegral $ h ^. dur
+    v = h ^. vol
+
+getConnection :: IO Connection
+getConnection = do
+    dstlist <- enumerateDestinations
+    case dstlist of
+      [] -> error "No MIDI Devices found."
+      (dst:_) -> openDestination dst
+
+calcDurations :: Composition -> Composition
+calcDurations comp = zipWith replaceDur comp durs'
+  where
+    durs  = map (view dur) comp
+    durs' = 0 : scanl1 (+) durs 
+    replaceDur h d = h & dur .~ d
+
+play :: Composition -> IO ()
+play comp = do
+    conn <- getConnection
+    start conn
+    evalStateT runComposition (conn, calcDurations comp)
+    close conn
+
+runComposition :: StateT (Connection, Composition) IO ()
+runComposition = do
+  (conn, comp) <- get
+  t <- lift $ currentTime conn
+  case comp of
+    [] -> return ()
+    (h:hs) -> do
+      let x@(MidiEvent s ev) = hitToMidiEvent h
+      when (s < t) $ do
+        put (conn, hs)
+        lift $ send conn ev
+      lift $ threadDelay 250
+      runComposition
--- src/Types.hs
+++ src/Types.hs
@@ -4,17 +4,60 @@ module Types where
 
 import Control.Lens
 
-data Instrument = Instrument
-    | Rest
-    | BassDrum
-    | SnareDrum
-    | HighHat
-    | CrashCymbal
+data Sound = 
+   BassDrum2
+ | BassDrum1
+ | SideStick
+ | SnareDrum1
+ | HandClap
+ | SnareDrum2
+ | LowTom2
+ | ClosedHihat
+ | LowTom1
+ | PedalHihat
+ | MidTom2
+ | OpenHihat
+ | MidTom1
+ | HighTom2
+ | CrashCymbal1
+ | HighTom1
+ | RideCymbal1
+ | ChineseCymbal
+ | RideBell
+ | Tambourine
+ | SplashCymbal
+ | Cowbell
+ | CrashCymbal2
+ | VibraSlap
+ | RideCymbal2
+ | HighBongo
+ | LowBongo
+ | MuteHighConga
+ | OpenHighConga
+ | LowConga
+ | HighTimbale
+ | LowTimbale
+ | HighAgogo
+ | LowAgogo
+ | Cabasa
+ | Maracas
+ | ShortWhistle
+ | LongWhistle
+ | ShortGuiro
+ | LongGuiro
+ | Claves
+ | HighWoodBlock
+ | LowWoodBlock
+ | MuteCuica
+ | OpenCuica
+ | MuteTriangle
+ | OpenTriangle
+    deriving (Show,Eq,Ord,Enum)
 
 data Hit = Hit
-    { _tone :: Instrument
+    { _tone :: Sound
     , _dur  :: Int
-    , _vol  :: Double
+    , _vol  :: Int
     }
 
 makeLenses ''Hit
