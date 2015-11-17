465ea3b668cfd5e42bf1a01af551da33bbb6036a
Reed Rosenbluth <Reed.Rosenbluth@gmail.com>
2015-11-17 15:57:55 -0500

    removed unnecessary import

--- src/Play.hs
+++ src/Play.hs
@@ -12,7 +12,6 @@ import System.MIDI
 import System.Info
 
 import Types
-import Interpret
 
 hitToMidiEvent :: Hit -> MidiEvent
 hitToMidiEvent h = MidiEvent d (MidiMessage 1 (NoteOn t v))
@@ -20,14 +19,14 @@ hitToMidiEvent h = MidiEvent d (MidiMessage 1 (NoteOn t v))
     t = 35 + fromEnum (h ^. tone)
     d = fromIntegral $ h ^. dur
     v = h ^. vol
-    
+
 getConnection :: IO Connection
 getConnection = do
     dstlist <- enumerateDestinations
     case dstlist of
       [] -> error "No MIDI Devices found."
       (dst:_) -> openDestination dst
-      
+
 play :: Composition -> IO ()
 play comp = do
     conn <- getConnection
