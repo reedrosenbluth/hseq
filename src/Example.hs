07ebe74f2614183afc9b36b0716f284eb37b6fd8
Reed Rosenbluth <Reed.Rosenbluth@gmail.com>
2015-11-17 15:57:31 -0500

    removed whitespace and added example

--- src/Example.hs
+++ src/Example.hs
@@ -1,16 +1,17 @@
 module Example where
-  
+
 import Data.Monoid
 
-import Types
 import Interpret
 import Drum
 import Play
 
 beat1 = mconcat [ mkComposition [hi, hi, hi, hi],
                   mkComposition [bd, sn, bd, sn] ]
-                
+
 bs = mkComposition $ cycle [bd, sn, bd, sn, bd, n8 sn, dot bd, sn]
+
+h2 = mkComposition $ replicate 2 (n4 hi)
 
 h8 = mkComposition $ replicate 8 (n8 hi)
 
@@ -22,7 +23,7 @@ hats = h8 <|> trill <|> h12 <|> trill <|> (mkComposition [n4 hi, hi, hi])
 
 trap = mconcat [ hats,
                  mkComposition [n1 bd, n1 sn, n1 bd, n1 sn]]
-                  
+
 house = mconcat [ mkComposition $ cycle [dot $ n8 rt, n4 hi]
                 , mkComposition $ cycle [n8 rt, n4 hi, n4 hi, n4 hi, n4 hi]
                 , mkComposition $ cycle [n4 bd, n4 bd, n4 bd, n4 bd]]
