60302bf802be564d875ee197814662d480bf9c97
Reed Rosenbluth <Reed.Rosenbluth@gmail.com>
2015-12-13 23:18:09 -0500

    clone and rest

--- src/Drum.hs
+++ src/Drum.hs
@@ -13,11 +13,21 @@ volume = 127
 note :: Rational -> Song -> Song
 note n = cmap (\h -> h & dur .~ 4 / n)
 
+-- | replicate a song a number of times
+clone :: Rational -> Song -> Song
+clone 1 s = s
+clone n s = s >> clone (n-1) s
+
 -- | modify the duration for each `Hit` in a `Song` to be
 -- 1.5 times as long
 dot :: Song -> Song
 dot = cmap (\h -> h & dur %~ (* 1.5))
 
+-- | quarter note rest
+rest :: Rational -> Song
+rest n = note n $ hsong (Hit BassDrum1 0 0)
+
+-- | notes of various durations
 n1, n2, n4, n8, n16, n32, n64 :: Song -> Song
 n1  = note 1
 n2  = note 2
@@ -26,6 +36,17 @@ n8  = note 8
 n16 = note 16
 n32 = note 32
 n64 = note 64
+
+-- | rests of various durations
+r1, r2, r4, r8, r16, r32, r64 :: Song
+r1  = rest 1
+r2  = rest 2
+r4  = rest 4
+r8  = rest 8
+r16 = rest 16
+r32 = rest 32
+r64 = rest 64
+
 
 -- | create an infinite loop of a `Song`
 loop :: Song -> Song
